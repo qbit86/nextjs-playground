c7b68a2025659c5dbc9c3db5fb5ed27d465bebe5
Viktor T <viktor.t@gmail.com>
2021-02-04 01:50:45 +0300

    Add title

--- pages/posts/test-post.js
+++ pages/posts/test-post.js
@@ -1,8 +1,12 @@
+import Head from 'next/head'
 import Link from 'next/link'
 
 export default function TestPost() {
   return (
     <>
+      <Head>
+        <title>Test post</title>
+      </Head>
       <h1>Test Post</h1>
       <h2>
         <Link href="/">
