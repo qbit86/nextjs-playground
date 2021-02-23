7add409540a849853074c246d629988d007c6d6d
Viktor Pti <qbit86@gmail.com>
2021-02-24 02:41:42 +0300

    Fix assetPrefix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 const isProd = process.env.NODE_ENV === 'production';
-const segment = 'nextjs-playground'
+const segment = 'nextjs-playground';
 
 module.exports = {
   basePath: isProd ? `/${segment}` : '',
-  assetPrefix: isProd ? `${segment}/` : ''
+  assetPrefix: isProd ? `/${segment}/` : ''
 };
